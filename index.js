ee1914973d111a24fdf470148740fde84c80745f
Harold Torres Marino <haroldtm55@gmail.com>
2021-05-17 23:45:02 +1000

    Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(driversArray,string){
+  return driversArray.filter(driver => {
+    return driver.toLowerCase() === string.toLowerCase()
+  })
+}
+
+function fuzzyMatch(driversArray,string) {
+  return driversArray.filter(driver => {
+    return driver.slice(0,Math.floor(driver.length/2)) === string
+  })
+}
+
+function matchName(driversArray,string) {
+  return driversArray.filter(({name}) => name === "Bobby")
+}
